826fb3c6a17a01f56e5c6bc8282da308aa1ce3f6
viraatmishraa <160683626+viraatmishraa@users.noreply.github.com>
2024-10-15 22:14:23 +0530

    Update passgen.py
    Some changes are made:
    1. Basically I added more randomness by removing some randomness (Steve Job's brilliant idea for randomizing i tunes)
    2. I have separated the validation part from the main program by constituting two separate functions. (basically validating input is a good habit at hand)
    3. instead of asking the user the number of passwords he/she wants, I created a sort of menu driven program to print as many function as they like indefinitely

--- passgen.py
+++ passgen.py
@@ -1,12 +1,59 @@
 import random
-print("WELCOME TO PASSWORD GENERATOR")
+lettersCap="QWERTYUIOPASDFGHJKLZXCVBNM"
+lettersSmall="qwertyuiopasdfghjklzxcvbnm"
+numbers="1234567890"
+specialChar="!@#$%^&*+-"
+# this function will tackle the exceptions 
+# in input that a user might enter
+def is_valid():    
+    try:
+        lenOfPass=int(input("ENTER LENGTH OF PASSWORD :"))
+        return lenOfPass
+    except:
+        print("enter valid input, try again!")
+        main()
+        
 
-passcodes='qwertyuioplkjhgfdsazxcvbnmm@#$%^&*()_+{}":>?<'
-numofpass=int(input("ENTER NO OF PASSWORDS YOU NEED :"))
-lenofpass=int(input("ENTER LENGTH OF PASSWORD :"))
-print("HERE IS YOUR PASSWORD ! / THANK YOU" )
-for i in range(numofpass):
+def main():
+    print("WELCOME TO PASSWORD GENERATOR")
+    lengthOfPass=is_valid()
+    #this condtion makes the password to fall in 
+    #strong category by excplicitly asking for a 
+    #password bigger than 5 chars.
+    if(lengthOfPass<5):
+        print("passwords smaller than 5 are invalid for \"strong password\" catgoery \ntry again")
+        main()
+
+
+    print("HERE IS YOUR PASSWORD ! / THANK YOU" )
+    charDecider=0
     word=""
-    for j in range(lenofpass):
-        word=word + random.choice(passcodes)
+    #instead on replying on random function which might
+    # return user a repetetive or weak password 
+    # out of pure luck, i explicitly made the password
+    # contain all four chars
+    # which makes the program on an average an edge
+    # over all 1,945,805,859,770,572,800 possibilities
+    # (approximately 2 quintillion)
+    for j in range(lengthOfPass):
+        if (charDecider==0):
+            word=word+random.choice(lettersSmall)
+        if (charDecider==1):
+            word=word+random.choice(lettersCap)
+        if(charDecider==2):
+            word=word+random.choice(numbers)
+        if(charDecider==3):
+            word=word+random.choice(specialChar)
+        if(charDecider==4):
+            charDecider=0
+        charDecider+=1
     print(word)
+    print("Do you want to continue? \ntype y if yes \ntype any key to exit")
+    if(input("Your response here:")=='y'):
+        main()
+    else
+        quit()
+main()
+
+        
+        
